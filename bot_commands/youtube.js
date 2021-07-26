d4063a0d1664b90c9302de5208922dd2d62877bf
WafiqAli <wafiqcali@gmail.com>
2021-07-27 06:02:33 +1200

    Update youtube.js : Added Comments and Code Tidy up
    Added lots of comments and cleaned up some code.

--- bot_commands/youtube.js
+++ bot_commands/youtube.js
@@ -1,18 +1,30 @@
+/**
+* @author : Wafiq Ali
+* Feature Name: YouTube Search and Play
+*
+*
+* @description : 
+* This file is used to execute a discord command that allows users to
+* to stream audio from YouTube videos and YouTube Playlists in a voice channel. Users are also 
+* able to search and output links to YouTube videos from the Top 10 search results from their search criteria (input).
+**/
+
 const Discord = require('discord.js');
 const search = require('youtube-search');
 const ytdl = require('ytdl-core');
 const ytpl = require('ytpl');
 const tokens = require('../tokens');
 
-const YOUTUBE_API_KEY = tokens.youtubeTokenAPI;
-const options = {
-    maxResults: 10,
+
+const YOUTUBE_API_KEY = tokens.youtubeTokenAPI;     //required to use YouTube Official API services such as youtube-search
+const options = { 
+    maxResults: 10,            // Gets the first 10 results from the search
     key: YOUTUBE_API_KEY,
     type: 'video'
 }
 
 let servers = {};
-let playing = false;
+let playing = false;        // Used to indicate whether the streamDispatcher (server.dispatcher) is currently streaming audio to a voice channel
 
 module.exports = {
     name: 'ytp',
@@ -20,8 +32,8 @@ module.exports = {
     async execute(message, args) {
         
         let embed = new Discord.MessageEmbed().setColor('#007E7D');
-        //let filter = m => m.author.id === message.author.id;
-        if (args.length == 0) {
+        
+        if (args.length == 0) {     // triggered if there was no input argument after the command "!ytp" 
             message.channel.send({
                 embed : {
                     title: 'YouTube Search and Play',
@@ -31,15 +43,16 @@ module.exports = {
             return;
         }
 
-        if (!servers[message.guild.id]) {
+        if (!servers[message.guild.id]) {       // Store a server by server ID. A key:value pair (server) is deleted once the Bot has finished streaming audio and disconnects.
             servers[message.guild.id] = {
-                queue : [],
+                queue : [],                     // Datastructure where each element is a video Object that includes information such as title, url/link, etc.
                 connection: [],
             }
         }
         let server = servers[message.guild.id];
 
-        if (args[0] === '-help') {
+        /* HELP COMMAND: Sends a message to help user understand how to use this feature */
+        if (args[0] === '-help') {             
             message.channel.send({
                 embed : {
                     title: 'YouTube Search and Play: Bot Commands',
@@ -56,15 +69,16 @@ module.exports = {
             return;
         }
 
+        /* PLAYLIST COMMAND: Takes in the URL of a YouTube Playlist and adds every video to the server's Queue. */
         if (args[0] === '-pl' && args[1]) {
             
-            if (!checkMemberInVoiceChannel()) {
+            if (!(await CheckMemberInVoiceChannel())) {
                 return;
             }
 
             let playlist = null;
             try {
-                playlist = await ytpl(args[1]);
+                playlist = await ytpl(args[1]);     // This package takes the playlist URL and returns a promise. Then it returns a datastructure with the information necessary to add every song to the queue.
             } catch (err) {
                 message.channel.send({
                     embed: {
@@ -75,23 +89,23 @@ module.exports = {
                 return;
             }
             
-            playlist.items.forEach(item => {
+            playlist.items.forEach(item => {       // adds every item to queue
                 item.link = item.shortUrl;
-                delete item.shortUrl;
+                delete item.shortUrl;              // Renaming a key from 'shortUrl' to 'link' to make my life easier later on
                 server.queue.push(item);
             });
 
             console.log(server.queue);
 
-            let isConnected = await checkBotConnection();
+            let isConnected = await CheckBotConnection();
             if (!isConnected) {
-                await connectBotToVoiceChannel();
+                await ConnectBotToVoiceChannel();
             }
 
             let connection = server.connection[0];
             
             if (!server.dispatcher) {
-                play(connection, message);
+                Play(connection, message);
             }
 
             message.channel.send({
@@ -104,9 +118,10 @@ module.exports = {
             return;
         }
 
+        /* DISPLAY QUEUE COMMAND: Sends a message in the corresponding text channel which displays a numbered list of the entire queue at the current time */
         if (args[0] === '-q') {
             
-            if (!checkMemberInVoiceChannel()) {
+            if (!(await CheckMemberInVoiceChannel())) {
                 return;
             }
 
@@ -135,18 +150,20 @@ module.exports = {
             return;
         }
 
+        /* SKIP CURRENT SONG COMMAND: Skips the current song that is playing and plays the next one in the queue */
         if (args[0] === '-s' && server.dispatcher) {
 
-            if (checkMemberInVoiceChannel()) {
+            if (await CheckMemberInVoiceChannel()) {
                 let connection = server.connection[0];
                 Skip(server, connection);
             }
             return;
         }
 
+        /* PAUSE/PLAY COMMAND: Pauses or Resumes the audio that is being streamed from the bot */
         if (args[0] === '-p' && server.dispatcher) {
             
-            if (!checkMemberInVoiceChannel()) {
+            if (!(await CheckMemberInVoiceChannel())) {
                 return;
             }
 
@@ -173,9 +190,10 @@ module.exports = {
             return;
         }
 
+        /* REMOVE A SONG COMMAND: Allows a user to remove a song from the queue from any position in the queue (given as input by user) */
         if (args[0] === '-r' && server.queue.length != 0) {
 
-            if (!checkMemberInVoiceChannel()) {
+            if (!(await CheckMemberInVoiceChannel())) {
                 return;
             }
 
@@ -194,21 +212,9 @@ module.exports = {
             }).catch(err => console.log(err));
            
 
-            let filter = m => (m.author.id === message.author.id) && m.content >= 1 && m.content <= server.queue.length;
+            let filter = m => (m.author.id === message.author.id) && m.content >= 1 && m.content <= server.queue.length; // filter used to ensure appropriate input from user
             let authorsMessage = await message.channel.awaitMessages(filter, {max: 1}).catch(err => console.log(err));
             const videoNum = authorsMessage.first().content;
-
-            if (server.queue[videoNum - 1] === server.queue[0]) {
-
-                message.channel.send({
-                    embed: {
-                        title: 'YouTube Search and Play',
-                        description: 'Please use "!ytp -s" to remove/skip the current song instead of "!ytp -remove"'
-                    }
-                }).catch(err => console.log(err));
-
-                return;
-            }
 
             message.channel.send({
                 embed: {
@@ -217,20 +223,21 @@ module.exports = {
                 }
             }).catch(err => console.log(err));
 
-            server.queue.splice(videoNum - 1, 1);
+            server.queue.splice(videoNum - 1, 1); // Removes an element using Array.prototype.splice() function.
 
             return;
         }
 
+        /* DISCONNECT BOT COMMAND: Disconnects the Bot from the voice channel that it is currently connected to */
         if (args[0] === '-d' && server.dispatcher) {
             
-            if (!checkMemberInVoiceChannel()) {
+            if (!(await CheckMemberInVoiceChannel())) {
                 return;
             }
 
             playing = false;
-            message.guild.me.voice.channel.leave();
-            delete servers[message.guild.id];
+            message.guild.me.voice.channel.leave();  // Disconnects from voice channel
+            delete servers[message.guild.id];       // Removes a server from the list of servers. When the bot is used again, it will add the server back.
 
             message.channel.send({
                 embed: {
@@ -242,9 +249,9 @@ module.exports = {
             return;
         }
 
-        //args.forEach((word) => { search_str += word + " ";});
-        const search_str = args.join(" ");
-        
+        /* REST OF CODE: Searches through YouTube and displays the results to the user. The user then picks a video, and then decides whether the video is 
+                         to be sent as a link in the corresponding text channel, added to the queue and played, or inserted into the queue and played.  */
+        const search_str = args.join(" ");    
         const results = await search(search_str, options).catch(err => console.log(err));
         
         if (results) {
@@ -264,7 +271,7 @@ module.exports = {
                 }
             }).catch(err => console.log(err));
             
-            let filter = m => (m.author.id === message.author.id) && m.content >= 1 && m.content <= youtubeResults.length;
+            let filter = m => (m.author.id === message.author.id) && m.content >= 1 && m.content <= youtubeResults.length; // filter ensures appropriate user input
             let authorsMessage = await message.channel.awaitMessages(filter, {max: 1}).catch(err => console.log(err));
             const videoNum = authorsMessage.first().content;
             
@@ -281,25 +288,24 @@ module.exports = {
             filter = m => (m.author.id === message.author.id) && (m.content === 'l' || m.content === 'p' || m.content === 'i');
             authorsMessage = await message.channel.awaitMessages(filter, {max: 1}).catch(err => console.log(err));
 
-            const videoData = youtubeResults[videoNum - 1];
-            const voiceChannel = message.member.voice.channel;
+            const videoData = youtubeResults[videoNum - 1];         // Retrieves and stores the chosen video from the array
             
-            if (authorsMessage) {
-                if (authorsMessage.first().content === 'l') {
+            if (authorsMessage) {                                   // Logic to determine what the user wants to do with their chosen video
+                if (authorsMessage.first().content === 'l') {       // User chose to send a link of the video url in text channel
                     console.log(authorsMessage.first().content);
                     message.channel.send(videoData.link);   
                 }
 
-                else if (authorsMessage.first().content === 'p') {
-                    if (!checkMemberInVoiceChannel()) {
+                else if (authorsMessage.first().content === 'p') {  // User chose to play the song (adds song to the queue if queue is not empty).
+                    if (!(await CheckMemberInVoiceChannel())) {
                         return;
                     }
 
                     PrepareToPlay(videoData);
                 }
 
-                else if (authorsMessage.first().content === 'i') {
-                    if (!checkMemberInVoiceChannel()) {
+                else if (authorsMessage.first().content === 'i') {  // User chose to insert the song at a specified position in the queue.
+                    if (!(await CheckMemberInVoiceChannel())) {
                         return;
                     }
                     
@@ -345,10 +351,18 @@ module.exports = {
             }
         } 
 
-        function checkMemberInVoiceChannel() {
+        /* Function CheckMemberInVoiceChannel() checks if the user who sent the message is in a voice channel, and if the user is in a voice channel, then it
+        *  also checks if the Bot is connected in the same voice channel. Returns True if user is in any voice channel while the bot is not 
+        *  connected at all OR if the user is in the same voice channel as the bot.
+        * 
+        *  @params -> None
+        *  @return -> {Boolean}  
+        **/
+        async function CheckMemberInVoiceChannel() {
             const voiceChannel = message.member.voice.channel;
-
-            if (!voiceChannel || !checkBotConnection()) {
+            const server = servers[message.guild.id];
+            
+            if (!voiceChannel || (!(await CheckBotConnection()) && server.connection[0])) {
                 message.channel.send({
                     embed: {
                         title: 'YouTube Search and Play',
@@ -361,20 +375,26 @@ module.exports = {
             return true;
         }
 
+        /* Function PrepareToPlay() is called when user attempts to add a song normally. This function adds the song to the queue and 
+        *  does some required checks to see if it is appropriate to call Play() or leave the song in the queue.
+        *
+        *  @params -> {videoData} This object holds information about a video, including its title and URL link.
+        *  @return -> None   
+        **/
         async function PrepareToPlay(videoData) {
             let server = servers[message.guild.id];
             let connection = null;
 
             server.queue.push(videoData);
             
-            let isConnected = await checkBotConnection();
+            let isConnected = await CheckBotConnection();
             if (!isConnected) {
-                await connectBotToVoiceChannel();
+                await ConnectBotToVoiceChannel();
             }
             connection = server.connection[0];
 
             if (!server.dispatcher) {
-                play(connection, message);
+                Play(connection, message);
             }
             else {
                 message.channel.send({
@@ -386,7 +406,12 @@ module.exports = {
             }
         }            
 
-        async function checkBotConnection() {
+        /* Function CheckBotConnection() checks if the bot is connected to the same voice channel as the user who sent the current message.
+        *
+        *  @params -> None
+        *  @return -> {Boolean}   
+        **/
+        async function CheckBotConnection() {
             const voiceChannel = message.member.voice.channel            
             const vcMembers = voiceChannel.members;
             
@@ -400,13 +425,24 @@ module.exports = {
             return isConnected;
         }
 
-        async function connectBotToVoiceChannel() {
+        /* Function ConnectBotToVoiceChannel() connects the bot to the same voice channel as the user who sent the current message.
+        *
+        *  @params -> None
+        *  @return -> None  
+        **/
+        async function ConnectBotToVoiceChannel() {
             const voiceChannel = message.member.voice.channel 
             let server = servers[message.guild.id];
             let connection = await voiceChannel.join();
             server.connection.push(connection);
         }
 
+        /* Function Skip() is called to skip the current song being played by calling OnEnd() function.
+        *
+        *  @params -> {server} Datastructure that holds the queue, connection and dispatcher objects
+        *  @params => {connection} current connection object between the bot and voice channel
+        *  @return -> None  
+        **/
         function Skip(server, connection) {
             message.channel.send({
                 embed: {
@@ -415,11 +451,17 @@ module.exports = {
                 }
             }).catch(err => console.log(err));
 
-            if (server.dispatcher) { onEnd(connection, message); };
+            if (server.dispatcher) { OnEnd(connection, message); };
             return;
         }
-       
-        async function play(connection, message) {
+        
+        /* Function Play() is called to stream audio from a video using ytdl() and a streamDispatcher object.
+        *
+        *  @params -> {connection} current connection between the bot and voice channel
+        *  @params -> {message} variable from main.js. Holds lots of valuable information
+        *  @return -> None  
+        **/
+        async function Play(connection, message) {
             let server = servers[message.guild.id];
             
             const stream = ytdl(server.queue[0].link, {filter : 'audioonly'});
@@ -435,26 +477,29 @@ module.exports = {
                     }
                 }).catch(err => console.log(err));
                 
-                server.dispatcher = connection.play(stream);
+                server.dispatcher = connection.Play(stream);
                 server.queue.shift();
             }
-            //server.dispatcher = connection.play(stream);
-            
+
             server.dispatcher.on("finish", () => {
-                //console.log("pog1");
-                //server.dispatcher = connection.play(stream);
-                onEnd(connection, message);
+                OnEnd(connection, message);
             });
         }
 
-        async function onEnd(connection, message) {
+        /* Function OnEnd() is called after a video has finished streaming. Either the next song is played or the bot disconnects (because empty queue).
+        *
+        *  @params -> {connection} current connection between bot and voice channel
+        *  @params -> {message} variable from main.js. Holds lots of valuable information
+        *  @return -> None 
+        **/
+        async function OnEnd(connection, message) {
             let server = servers[message.guild.id];
             
             playing = false;
 
             console.log(server.queue)
             if (server.queue.length != 0) {
-                play(connection, message);
+                Play(connection, message);
 
             } else {
                 connection.disconnect();
