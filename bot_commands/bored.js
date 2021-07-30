7d21342c3bfc02eb4217374d7753c87de891aed9
WafiqAli <wafiqcali@gmail.com>
2021-07-30 23:20:58 +1200

    Update bored.js: Removing selection feature
    I was going to allow a user to refine their activity search by choosing their own property values such as type, participants, price, accessibility etc. However, I don't think its worth it for now. Might change my mind later but this feature will remain at this complexity for now.

--- bot_commands/bored.js
+++ bot_commands/bored.js
@@ -1,46 +1,23 @@
 const fetch = require('node-fetch');
 
+
 module.exports = {
     name: 'bored',
     description: 'Uses an API which retrieves possible things you could do when you are bored',
     
     async execute(message, args) {
-        
-        message.channel.send({
-            embed : {
-                title: 'Unbored the Bored',
-                description: 'Welcome to Unbored the Bored, a service to help you find something to do!'
-            }
-        });
-        message.channel.send({
-            embed : {
-                title: 'Unbored the Bored: Select an Option',
-                description: 'Type "-r" for a random activity or "-p" to pick something more specific!'
-            }
-        });
-        
-        let filter = m => (m.author.id === message.author.id) && (m.content === '-r' || m.content === '-p'); // filter used to ensure appropriate input from user
-        const authorsMessageData = await message.channel.awaitMessages(filter, {max: 1}).catch(err => console.log(err));
-        let authorsMessage = authorsMessageData.first().content;
+         
+        fetch("https://www.boredapi.com/api/activity", 
+        {
+            headers : {
+                Accept: "application/json",
+            },
+        })
+        .then(response => response.json())
+        .then(data => RandomActivity(data));
 
-        if (authorsMessageData) {
-            if (authorsMessage === '-r') {
-                
-                fetch("https://www.boredapi.com/api/activity", 
-                {
-                    headers : {
-                        Accept: "application/json",
-                    },
-                })
-                .then(response => response.json())
-                .then(data => RandomActivity(data));
-        
-            }
-            else if (authorsMessage === '-p') {
-                SelectActivity();
-            }
-        }
-        
+
+
         function getAccessibilityString(data) {
             if (data.accessibility <= 0) {
                 return '\n**Accessibility**: {' + data.accessibility + '} Easily Accessible, most people should be able to do this!' 
@@ -58,48 +35,48 @@ module.exports = {
                 return '\n**Accessibility**: {' + data.accessibility + '} GOODLUCK MATE! THIS MIGHT BE IMPOSSIBLE...' 
             }
         }
-
+    
         function getPriceString(data) {
             if (data.price <= 0) {
                 return '\n**Price**: {' + data.price + '} This activity is essentially FREE!!!' 
-             }
-             else if (data.price <= 0.3) {
+            }
+            else if (data.price <= 0.3) {
                 return '\n**Price**: {' + data.price + '} A very cheap activity...' 
-             }
-             else if (data.price <= 0.5) {
+            }
+            else if (data.price <= 0.5) {
                 return '\n**Price**: {' + data.price + '} Should be affordable for most people...' 
-             }
-             else if (data.price <= 0.8) {
+            }
+            else if (data.price <= 0.8) {
                 return '\n**Price**: {' + data.price + '} Quite an expensive activity...' 
-             }
-             else if (data.price <= 1.0) {
+            }
+            else if (data.price <= 1.0) {
                 return '\n**Price**: {' + data.price + '} YOU BETTER SAVE LOTS OF MONEY BECAUSE THIS WILL BURN YOUR WALLET!' 
-             }
+            }
         }
-
+    
         function RandomActivity(data) {
             
             let messageStr = '';
-
+    
             messageStr +='**Activity**: ' + data.activity + '\n**Type**: ' + data.type + '\n**Participants**: ' + data.participants;
             messageStr += getPriceString(data);
             messageStr += getAccessibilityString(data);
             if (data.link) {
                 messageStr += '\n**For More Information**: ' + data.link;
             }
-
+    
             message.channel.send({
                 embed : {
-                    title: 'Unbored the Bored: Your Activity',
+                    title: 'Random Activity',
                     description: messageStr
                 }
             });
-
         }
+    }  
+}   
+    
 
-        function SelectActivity() {
+    
 
-        }
-        
-    }    
-}
+      
+
